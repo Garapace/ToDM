689b1424126a95dee1ac9ff8d6a14c03b952d090
Garapace <starkov-2003@list.ru>
2025-11-24 21:43:46 +0500

    lab 1 modify

--- lab1/main.py
+++ lab1/main.py
@@ -1,14 +1,16 @@
-# константы
 import math
 
 
+# константы
 EPSILON = 1e-6    # заданная точность 10^-6
 DELTA = EPSILON/2 # допустимая погрешность
 iterations = 0    # количество итераций для метода
 
+
 def function(x):
     """ f(x) = x^2/2 - cos(x)   """
     return x**2/2 - math.cos(x)
+
 
 def function_first_derivative(x):
     """ Первая произодная функции
@@ -16,28 +18,13 @@ def function_first_derivative(x):
         f'(x) = sin(x) + x      """
     return math.sin(x) + x
 
+
 def function_second_derivative(x):
     """ Вторая производная функции
         f(x)   = x^2/2 - cos(x)
         f''(x) = cos(x) + 1     """
     return math.cos(x) + 1
 
-def method_dichotomy(a, b):
-    """ Метод деления пополам (метод бисекции | метод дихотомии)"""
-    global iterations
-    iterations = 0
-    
-    while (abs(b - a) > EPSILON):
-        x1 = (a + b - DELTA) / 2
-        x2 = (a + b + DELTA) / 2
-        
-        if function(x1) >= function(x2):
-            a = x1
-        else:
-            b = x2
-        iterations += 1
-    
-    return (a + b) / 2
 
 def method_dichotomy(a, b):
     """ Метод деления пополам (метод бисекции | метод дихотомии)"""
@@ -55,6 +42,26 @@ def method_dichotomy(a, b):
         iterations += 1
     
     return (a + b) / 2
+
+
+def method_dichotomy(a, b):
+    """ Метод деления пополам (метод бисекции | метод дихотомии)"""
+    global iterations
+    iterations = 0
+    
+    while (abs(b - a) > EPSILON):
+        x1 = (a + b - DELTA) / 2
+        x2 = (a + b + DELTA) / 2
+        
+        if function(x1) >= function(x2):
+            a = x1
+        else:
+            b = x2
+        iterations += 1
+    
+    return (a + b) / 2
+
+
 def method_dichotomy(a, b):
     """ Метод деления пополам (метод бисекции | метод дихотомии)"""
     global iterations
